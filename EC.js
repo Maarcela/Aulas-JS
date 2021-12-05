f64a7c6807b453e8a362a14224bc783581125903
Maarcela <marcela.vitoria277@gmail.com>
2021-12-05 20:12:26 -0300

    Estrutura condicionais js

--- EC.js
+++ EC.js
@@ -3,29 +3,30 @@ if ( a => * 0.20 ) {
  console.log( "Os valores são iguais" );
 } */
 
-
 // Estrutura condicional composta, atividade 1
- function aumentoSalario ( Salário = 280, aumento, novoSalario) {
-if (Salário <= 280 ) {
-    aumento = (Salário * 20 / 100);
-} else if ( Salário > 280  && Salário <= 700){
-    aumento = ( Salário * 15 / 100);
-}  else if ( Salário > 700  && Salário <= 1500){
-    aumento = ( Salário * 10 / 100);
-}else { aumento = (Salário * 5 / 100)
+function aumentoSalario(Salário = 280, aumento, novoSalario) {
+    if (Salário <= 280) {
+        aumento = (Salário * 20) / 100;
+    } else if (Salário > 280 && Salário <= 700) {
+        aumento = (Salário * 15) / 100;
+    } else if (Salário > 700 && Salário <= 1500) {
+        aumento = (Salário * 10) / 100;
+    } else {
+        aumento = (Salário * 5) / 100;
+    }
+    return (novoSalario = Salário + aumento)
 }
- return (Salário + aumento + novoSalario);
+console.log(aumentoSalario());
+ 
+// Atividade 2
+console.clear()
+function maiorNumero(num1 = 5, num2 = 8, num3 = 15) {
+    if (num2 > num1 && num2 > num3) {
+        return `Número ${num2} é maior`;
+    } else if (num2 > num1 && num2 > num3) {
+        return ` Número ${num3} é maior`;
+    } else if (num3 > num1) {
+        return `Número ${num3} é maior`;
+    }
 }
- console.log (' O salário novo do colaborador é:');
- console.log (aumentoSalario ());
-
- // Atividade 2
-
- function maiorNumero ( num1 = 5 , num2 = 8, num3 = 15){
-     if( num1 < num2 && num2 > num3);
-     return num2;
- } 
-
-
-
-
+console.log(maiorNumero());
