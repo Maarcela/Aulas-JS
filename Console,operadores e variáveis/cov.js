6dce80dbedfed371fab4a3ee058a86790469cb88
Maarcela <marcela.vitoria277@gmail.com>
2021-12-05 18:50:15 -0300

    Console, operadores e variáveis

---
+++ Console,operadores e variáveis/cov.js
@@ -0,0 +1,9 @@
+//Atividade 1
+let nome = 'Juliana'
+let peso = 8.9
+let altura = 1.70
+let imc = peso / altura
+
+const mensagem = nome  + ' possui altura ' +  altura  + ' pesa '  +  peso  + ', seu IMC é ' + imc
+
+console.log(mensagem);
