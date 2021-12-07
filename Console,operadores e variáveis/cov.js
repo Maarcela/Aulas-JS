f3bdfd77c1c1d12a505f274ed20f0b0556d597a8
Maarcela <91353886+Maarcela@users.noreply.github.com>
2021-12-07 15:41:51 -0300

    Delete Console,operadores e variáveis directory

--- Console,operadores e variáveis/cov.js
+++
@@ -1,9 +0,0 @@
-//Atividade 1
-let nome = 'Juliana'
-let peso = 8.9
-let altura = 1.70
-let imc = peso / altura
-
-const mensagem = nome  + ' possui altura ' +  altura  + ' pesa '  +  peso  + ', seu IMC é ' + imc
-
-console.log(mensagem);
